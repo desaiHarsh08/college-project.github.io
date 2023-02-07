7ae4b7aa802bdd57912bd0af01b49128a5bf2a7c
desaiHarsh08 <desaiharsh183@gmail.com>
2023-02-07 12:26:57 +0530

    National Adv. Updated

--- src/components/NationalAdvisory.js
+++ src/components/NationalAdvisory.js
@@ -13,19 +13,23 @@ const NationalAdvisory = () => {
       <div className='cards '>
         <div className='row my-5 flex flex-col sm:flex-row gap-7'>
           <div className='card shadow-xl  hover:bg-red-600 hover:text-white w-full sm:w-1/3 flex flex-col justify-center items-center  py-7 border-2 bg-white '>
-            <span className='text-sm lg:text-xl'>Dr. Ankit Bhurane</span>
-            <span className='text-xs lg:text-base'>IIIT, Nagpur</span>
+            <span className='text-sm lg:text-xl'>Dr. Santosh Chide</span>
+            <span className='text-xs lg:text-base'>Principal D.Y.P.C.O.E.T, Kolhapur</span>
           </div>
           <div className='card shadow-xl  hover:bg-red-600 hover:text-white w-full sm:w-1/3 flex flex-col justify-center items-center  py-7 border-2 bg-white '>
             <span className='text-sm lg:text-xl'>Dr. PM Padole</span>
             <span className='text-xs lg:text-base'>Director VNIT, Nagpur</span>
           </div>
           <div className='card shadow-xl  hover:bg-red-600 hover:text-white w-full sm:w-1/3 flex flex-col justify-center items-center  py-7 border-2 bg-white '>
-            <span className='text-sm lg:text-xl'>Dr. Sudhir </span>
+            <span className='text-sm lg:text-xl'>Dr. Sudhir Akojwar </span>
             <span className='text-xs lg:text-base'>G.C.O.E, Nagpur</span>
           </div>
         </div>
         <div className='row my-5 flex flex-col sm:flex-row gap-7'>
+          <div className='card shadow-xl  hover:bg-red-600 hover:text-white w-full sm:w-1/3 flex flex-col justify-center items-center  py-7 border-2 bg-white '>
+            <span className='text-sm lg:text-xl'>Dr. Ankit Bhurane</span>
+            <span className='text-xs lg:text-base'>IIIT, Nagpur</span>
+          </div>
           <div className='card shadow-xl  hover:bg-red-600 hover:text-white w-full sm:w-1/3 flex flex-col justify-center items-center  py-7 border-2 bg-white '>
             <span className='text-sm lg:text-xl'>Dr. P.B. Dhaigavane</span>
             <span className='text-xs lg:text-base'>Jr. Director, DTE.</span>
@@ -34,16 +38,24 @@ const NationalAdvisory = () => {
             <span className='text-sm lg:text-xl'>Dr. H.O Thakre</span>
             <span className='text-xs lg:text-base'>IE(I), Nagpur</span>
           </div>
+        </div>
+        <div className='row my-5 flex flex-col sm:flex-row gap-7'>
+          
           <div className='card shadow-xl  hover:bg-red-600 hover:text-white w-full sm:w-1/3 flex flex-col justify-center items-center  py-7 border-2 bg-white '>
             <span className='text-sm lg:text-xl'>Dr. K.M. Burchandi</span>
             <span className='text-xs lg:text-base'>VNIT, Nagpur</span>
           </div>
-        </div>
-        <div className='row my-5 flex flex-col sm:flex-row gap-7'>
           <div className='card shadow-xl  hover:bg-red-600 hover:text-white w-full sm:w-1/3 flex flex-col justify-center items-center  py-7 border-2 bg-white '>
             <span className='text-sm lg:text-xl'>Dr. R.P. Borker</span>
             <span className='text-xs lg:text-base'>Principal, G.C.O.E. Nagpur</span>
           </div>
+          <div className='card shadow-xl invisible hover:bg-red-600 hover:text-white w-full sm:w-1/3 flex flex-col justify-center items-center  py-7 border-2 bg-white '>
+            <span className='text-sm lg:text-xl'>Dr. R.P. Borker</span>
+            <span className='text-xs lg:text-base'>Principal, G.C.O.E. Nagpur</span>
+          </div>
+        </div>
+        <div className='row my-5 flex flex-col sm:flex-row gap-7'>
+          
         </div>
       </div>
     </div>
